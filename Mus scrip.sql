04db8d3e7eaf3f6a26bd3890734378d3ea5c3ade
pounson <89928500+pounson@users.noreply.github.com>
2021-12-03 16:49:46 +0300

    corrected
    corrected work

--- Mus scrip.sql
+++ Mus scrip.sql
@@ -1,45 +1,48 @@
-create table if not exists Album(
-	Id serial primary key,
-	Name varchar(100) not null
+create table if not exists artist (
+	id serial primary key,
+	name varchar(60) not null
 );
 
-create table if not exists Track(
-	Id serial primary key,
-	Name varchar(100) not null,
-	AlbumId integer references Album(Id)
+
+create table if not exists genre (
+	id serial primary key,
+	name varchar(60) not null unique
 );
 
-create table if not exists Genre(
-	Id serial primary key,
-	Name varchar(35) not null
+create table if not exists artist_genre(
+	id serial primary key,
+	genre_id integer not null references genre(id),
+	artist_id integer not null references artist(id)
 );
 
-create table if not exists artist(
-	Id serial primary key,
-	Name varchar(35) not null unique,
-	GenreId integer references Genre(Id) not null
+create table if not exists album (
+	id serial primary key,
+	name varchar(60) not null,
+	releaseDate integer not null
 );
 
-create table if not exists Artist_Genre(
-	Id serial primary key,
-	ArtistId integer references Artist(Id) not null,
-	GenreId integer references Genre(Id) not null
+create table if not exists album_artist (
+	id  serial primary key,
+	artist_id integer not null references artist(id),
+	album_id integer not null references album(id)
 );
 
-create table if not exists Artist_Album(
-	Id serial primary key,
-	ArtistId integer references Artist(Id) not null,
-	AlbumId integer references Album(Id) not null
+create table if not exists track (
+	id serial primary key,
+	name varchar(60) not null,
+	trackLength numeric(3,2) not null,
+	album_id integer references album(id) 
 );
 
-create table if not exists Collection(
-	Id serial primary key,
-	Name varchar(100) not null,
-	Year numeric(4,0) not null
+create table if not exists collections(
+	id serial primary key,
+	name varchar(60) not null,
+	year integer  not null
 );
 
-create table if not exists Collection_Tracks(
-	Id serial primary key,
-	CollectionId integer references Collection(Id) not null,
-	TrackId integer references Track(Id) not null
+create table if not exists collections_album_track(
+	id serial primary key,
+	collections_id integer not null references collections(id),
+	album_id integer not null references album(id),
+	track_id integer not null references track(id)
 );
